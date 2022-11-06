8006e517a1ee7ebb95050c90d003913db4ac2806
Jonathan Vicente <jonathan-priston@hotmail.com>
2022-11-06 20:25:36 -0300

    Implementando detalhe

--- paginas/perfilPrestador/perfilPrestador.php
+++ paginas/perfilPrestador/perfilPrestador.php
@@ -2,8 +2,6 @@
 
 include("../connect.php");
 
-
-
 session_start();
 
 if (!isset($_SESSION['cdo_email'])) {
@@ -97,7 +95,13 @@ $sqlQuery = $conexao->query("SELECT * FROM arquivos") or die($conexao->error);
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-
+    <script type="text/javascript">
+     $(window).bind("beforeunload", function() {
+       if ($("#myModal").is(":visible")) {
+           return "Você não salvou a sua tarefa, gostaria mesmo de sair?";
+       }
+    });
+    </script>
 
     <script type="text/javascript">
     function ajax() {
@@ -316,7 +320,7 @@ $sqlQuery = $conexao->query("SELECT * FROM arquivos") or die($conexao->error);
 
                                         </div>
 
-                                        <form method="POST" action="">
+                                        <form method="POST">
                                              
                                             <textarea name="mensagem" placeholder="Insira uma mensagem"></textarea>
                                             <input type="submit" name="enviar" value="Enviar">
