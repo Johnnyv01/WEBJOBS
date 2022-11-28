5055e8c27f13512f2eedeff652408005281da540
Jonathan Vicente <jonathan-priston@hotmail.com>
2022-11-27 21:51:23 -0300

    Solicitando serviço via whatsapp

--- paginas/eletricista.php
+++ paginas/eletricista.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/solicitarServico.css">
     <link rel="stylesheet" media="screen and (max-width: 768px)" href="/paginas/css/mobile.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
     <title>Document</title>
 </head>
 
@@ -26,7 +28,7 @@
         <div id="eletricista-container">
             <div class="eletricista">
                 <i class="fa-solid fa-user-tie fa-3x"></i>
-                <span class="eletricista-tittle">Solicitar eletricista </span> <br>
+                <span class="eletricista-tittle">Solicita eletricista </span> <br>
                 <p>Informe sua necessidade e os profissionais mais adequados e qualificados entrarão em contato com você
                     <br><br> </p>
                 <h3>Informe a sua necessidade?<br><br> </h3>
@@ -47,10 +49,110 @@
                         <span class="limit_numm">/500</span>
                     </div>
                 </div>
-                <button class="my-button" onclick="alert('solicitacao enviada!')"> Clique para solicitar</button>
+                <button class="my-button" onclick="location.href='solicitandoServico.php'"> Clique para solicitar</button>
 
             </div>
     </section>
+
+         <!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Conecte-se com as nossas redes sociais:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>WEB JOBS
+          </h6>
+          <p>
+            Ficou alguma duvida, entre em contato vamos adorar te conhecer
+          </p>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+           MENU
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Sobre nos</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Serviços</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Entrar</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> São paulo, SP 08253-000, BR</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            webjobs@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 55 9234 567 88</p>
+         
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Webjobs.com.br</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+
 
     <div id="footer">
         <p>Copyright 2022 - Todos os direitos reservados</p>
--- paginas/solicitandoServico.php
+++ paginas/solicitandoServico.php
@@ -5,80 +5,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/solicitarServico.css">
     <link rel="stylesheet" media="screen and (max-width: 768px)" href="/paginas/css/mobile.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
-    <title>WEBJOBS</title>
+    <title>Solicitar Serviço</title>
 </head>
 
 <body>
     <nav id="navbar">
         <div id="navbar-container">
-            <h3> <a href="index.php"> WEB JOBS </a></h3>
+            <h1> <a href="index.php">WEB JOBS </a></h1>
             <ul id="navbar-items">
-                <li><a href="sobrenos.php"> Sobre Nós </a></li>
-                <li><a href="servicos.php"> Serviços </a></li>
-                <li><a href="login.php?variavel">Entrar </a></li>
+                <li><a href="sobrenos.php">Sobre Nós </a></li>
+                <li><a href="servicos.php">Serviços </a></li>
+                <li><a href="login.php">Entrar </a></li>
+                <li><a href="loginPrestador.php">Ofereça o seu serviço</a></li>
             </ul>
         </div>
     </nav>
+    <section id="eletricista">
+        <div id="eletricista-container">
+            <div class="eletricista">
+                <i class="fa-solid fa-user-tie fa-3x"></i>
+                <span class="eletricista-tittle">Solicitando eletricista </span> <br>
+                <h6>Informe o seu numero WhatsApp
+                    <br><br> </h6>
+                    <div class="input-box1">
+                    <img src="../imagens/img/brazil-icon.jpg" alt=""> <p>+55</p>
+                    <input type="text">
+                    </div>
+                <button class="my-button" onclick="alert('solicitacao enviada!')"> Clique para solicitar</button>
 
-    <section id="mvl">
-        <div class="headline">WEBJOBS</div>
-        <div id="mvl-container">
-            <H1>Quem somos?</H1>
-            <p>Somos idealistas de uma nova forma de interação entre o o prestador de serviço e o cliente, uma
-                plataforma inovadora que oferece para todos os melhores profissionais para ajudar você a realizar seus
-                projetos e sonhos sem dificuldades.</p><br><br><br>
-
-            <H1>Visão</H1>
-            <p>A webjobs nasceu da necessidade de agir e ajudar as pessoas que precisam e querem estar presentes no
-                mercado de trabalho ou fazer renda extra, foi assim que nós idealizamos uma forma de trazer aos nossos
-                usuários o compromisso de ofertar os seus serviços para aqueles que procuram por um profissional para
-                atender as suas necessidades. Queremos inovar o modelo de interação e o compromisso de serviços entre
-                pessoas físicas e jurídicas de forma simples e fácil. </p><br>
-
-            <H1>Valores</H1>
-            <p>O compromisso com os nossos clientes é fundamental para que haja confiança e seriedade em nossos
-                serviços,
-                garantindo a satisfação os nossos clientes e qualidade naquilo que fazemos. </p>
-        </div>
+            </div>
     </section>
 
-    <div id="devs">
-        <div class="headline">NOSSO TIME</div>
-        <div id="devs-container">
-            <div class="dev">
-                <img src="../imagens/img/engenheiro.png" alt="" class="dev-image">
-                <h1>Jonathan Vicente</h1><br>
-                <p>Desenvolvedor de Software</p>
-            </div>
-            <div class="dev">
-                <img src="../imagens/img/engenheiro.png" alt="" class="dev-image">
-                <h1>Kauan Alexandre</h1><br>
-                <p>Programador web</p>
-            </div>
-            <div class="dev">
-                <img src="../imagens/img/engenheiro.png" alt="" class="dev-image">
-                <h1>Gabriel Freitas</h1><br>
-                <p>Analista de sistemas</p>
-            </div>
-            <div class="dev">
-                <img src="../imagens/img/engenheiro.png" alt="" class="dev-image">
-                <h1>Thaís Huertas</h1><br>
-                <p>Analista de sistemas</p>
-            </div>
-            <div class="dev">
-                <img src="../imagens/img/engenheiro.png" alt="" class="dev-image">
-                <h1>Rian Neris</h1><br>
-                <p>DBA</p>
-            </div>
-        </div>
-    </div>
-</body>
-
-<!-- Footer -->
+     <!-- Footer -->
 <footer class="text-center text-lg-start bg-light text-muted">
   <!-- Section: Social media -->
   <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
@@ -177,8 +139,6 @@
   <!-- Copyright -->
 </footer>
 
-<div id="footer">
-        <p>Copyright 2022 - Todos os direitos reservados</p>
-    </div>
+</body>
 
 </html>
